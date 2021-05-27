d20e5bd5844de0473d6ff1ea1732bb33035213e0
aryan1090 <aryanchowdhary2172@gmail.com>
2021-05-27 21:02:25 +0530

    Home,Shop and SignIn Pages done

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,48 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCCSM7aMGk4QcR1CWLhLjwJsmYjisQjN3g",
+    authDomain: "crownserver-4e5ac.firebaseapp.com",
+    projectId: "crownserver-4e5ac",
+    storageBucket: "crownserver-4e5ac.appspot.com",
+    messagingSenderId: "1074036329853",
+    appId: "1:1074036329853:web:6d634a427faa93ae109014"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
