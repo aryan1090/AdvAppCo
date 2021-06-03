5de3064340b4e1bddfcb9f21c6cace483614e339
aryan1090 <aryanchowdhary2172@gmail.com>
2021-06-03 20:52:50 +0530

    made some changes in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,9 +10,13 @@ import {
   selectCartTotal
 } from '../../redux/cart/cart.selectors';
 
+import {selectCurrentUser} from '../../redux/user/user.selectors';
+import CustomButton from '../../components/custom-button/custom-button.component';
+
+import { Link } from 'react-router-dom';
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
+const CheckoutPage = ({ cartItems, total,currentUser }) => (
   <div className='checkout-page'>
     <div className='checkout-header'>
       <div className='header-block'>
@@ -31,22 +35,48 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <div className='total'>TOTAL: ${total}</div>
-    <div className='test-warning'>
+    {cartItems.length!==0?
+      (
+        cartItems.map(cartItem => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))
+      )
+      :
+      
+        <h1>Your cart is Empty!</h1>
+      
+    }
+    {cartItems.length!==0?(
+      <div className='total'>TOTAL: ${total}</div>
+    ):(
+      <span></span>
+    )}
+    
+    {/* <div className='test-warning'>
       *Please use the following test credit card for payments*
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-    </div>
-    <StripeCheckoutButton price={total} />
+    </div> */}
+    {currentUser ? (
+      <StripeCheckoutButton price={total} /> )
+      :
+        (// (<Link className='option' to='/signin'>
+        //   SIGN IN TO CHECKOUT
+        // </Link>
+        <CustomButton inverted>
+        <Link className = 'option' to='/signin'>
+         SIGN IN TO CHECKOUT
+         </Link>
+      </CustomButton>
+      ) }
+    {/* <StripeCheckoutButton price={total} /> */}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  total: selectCartTotal
+  total: selectCartTotal,
+  currentUser: selectCurrentUser
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
